b7365124fd72124c1a8c9f548529cc8834865e7e
Viktor Hangya <hangyav@inf.u-szeged.hu>
2023-05-21 13:15:16 +0200

    v0.2.0 and pypi workflow

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="textLSP",
-    version="0.1.0",
+    version="0.2.0",
     author="Viktor Hangya",
     author_email="hangyav@gmail.com",
     description=("Language server for text spell and grammar check with various tools."),
